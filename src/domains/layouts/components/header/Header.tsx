01d4a73424da050b079d1fa3e464c1772266f036
Kirill Zharov <zharov_kir@hotmail.com>
2020-06-27 01:11:30 +0700

    add link to Header component

--- src/domains/layouts/components/header/Header.tsx
+++ src/domains/layouts/components/header/Header.tsx
@@ -1,17 +1,31 @@
 import React from 'react';
-import { AppBar, Toolbar, IconButton, Typography } from '@material-ui/core';
+import { Link } from 'react-router-dom';
+import { AppBar, Toolbar, IconButton, Typography, makeStyles } from '@material-ui/core';
 
 import MenuIcon from '@material-ui/icons/Menu';
 
-export const Header: React.FC = () => (
-    <AppBar position='static'>
-        <Toolbar>
-            <IconButton edge='start' color='inherit' aria-label='menu'>
-                <MenuIcon />
-            </IconButton>
-            <Typography variant='h6' color='inherit' noWrap>
-                Translate API Client
-            </Typography>
-        </Toolbar>
-    </AppBar>
-);
+export const Header: React.FC = () => {
+    const classes = useStyles();
+
+    return (
+        <AppBar position='static'>
+            <Toolbar>
+                <IconButton edge='start' color='inherit' aria-label='menu'>
+                    <MenuIcon />
+                </IconButton>
+                <Link to='/' className={classes.link}>
+                    <Typography variant='h6' color='inherit' noWrap>
+                        Translate API Client
+                    </Typography>
+                </Link>
+            </Toolbar>
+        </AppBar>
+    );
+};
+
+const useStyles = makeStyles(() => ({
+    link: {
+        color: '#fff',
+        textDecoration: 'none',
+    },
+}));
